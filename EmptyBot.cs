3603d6b60aec0653c6035f35b0d495221356a046
zincri <zincri_mlz@hotmail.com>
2020-08-05 02:28:52 -0500

    Zincri
    Chatbot

--- EmptyBot.cs
+++ EmptyBot.cs
@@ -30,37 +30,105 @@ namespace ValesBot
             var message_received = turnContext.Activity.Text;
             var array_message = message_received.Split(' ');
 
-            //string[,] matriz = new string[2,4];
             string[][] matriz = new string[2][];
-            matriz[0] = new string [] {"El apoyo Vales grandeza es un programa del gobierno de gto.",
-            "que",
-            "trata",
-            "apoyo"};
-/*             matriz[1,0] = "Los vales tienen el valor de 50 pesos c/u.";
-            matriz[1,1] = "que";
-            matriz[1,2] = "valor";
-            matriz[1,3] = "tiene";
-            matriz[1,3] = "vale"; */
+            matriz[0] = new string [] {"El apoyo Vales grandeza es un programa del gobierno de gto."
+            ,"que"
+            ,"trata"
+            ,"apoyo"};
 
-            int contador=0;
+            matriz[1] = new string []  {"Los vales tienen el valor de 50 pesos c/u."
+            ,"que"
+            ,"valor"
+            ,"tiene"
+            ,"vale"};
+
+            List<int> array_match = new List<int>();
+            int indice = 1;
             foreach (var item in array_message)
             {
-                var x = matriz[0];
-                foreach (var words in matriz)
-                {
-                    Console.Write(words);
-                    /* foreach (var word in words)
-                    {
-                        if(String.Equals(word, item, StringComparison.CurrentCultureIgnoreCase)){
-
-                        }
-                    }  */
-                    
-                    
-                } 
+                int elementos_en_pila = array_match.Count;
                 
-            }            
-            await turnContext.SendActivityAsync($"El usuario dijo: {message_received}",cancellationToken: cancellationToken);
+                //Se necesita una bandera para que solo la primera vez pase en el for de abajo,
+                //despues solo busque la palabra en otro for donde se itere la matriz[matchs]
+                //Y cuando un match ya no continue, entonces se eliminara de la pila de matchs,
+                //Cuando el array tenga solo un elemento es entonces que encontro el match exacto.
+
+                //Tambien debes validar cuando no haya ningun match y debe buscar en otra palabra(posicion.)
+                if (indice==1){
+                    int contador=0;
+                    foreach (var words in matriz)
+                    {
+                        if(String.Equals(words[indice], item, StringComparison.CurrentCultureIgnoreCase)){
+                            array_match.Add(contador);
+                        }
+                        contador++;
+                    }
+
+                }
+                else{
+                    //Usar un for para iterar solo 
+                    //los que si hicieron match,
+                    //Tambien se necesita un contador
+                    //para saber que palabra de lo que
+                    //ingreso el usuario se esta comparando.
+
+                    //Si llenamos el array de match con los mas repetidos
+                    //es mas facil determinar cual es la respuesta
+                    int[] array_match_temporal= new int[array_match.Count];
+                    array_match.CopyTo(array_match_temporal,0);
+
+                    for (int i = 0; i < array_match_temporal.Length; i++)
+                    {
+                        if(String.Equals(matriz[array_match_temporal[i]][indice], item, StringComparison.CurrentCultureIgnoreCase)){
+                            array_match.Add(array_match_temporal[i]);
+                        }
+                    }
+
+                }
+                if(array_match.Count != elementos_en_pila){
+                    indice++;
+                }    
+            }
+            if(array_match.Count==0){
+                await turnContext.SendActivityAsync("No encontre informacion suficiente para responder tu pregunta.",cancellationToken: cancellationToken);
+            }
+            else{
+                int numero_veces=0;
+                int[,] numeros_repetidos = new int[array_match.Count,2];
+                for (int i = 0; i < array_match.Count; i++)
+                {
+                    for (int j = 0; j < array_match.Count; j++)
+                    {
+                        if(array_match[i]==array_match[j]){
+                            numero_veces += 1;
+                        }
+                    }
+                    numeros_repetidos[i,0]=array_match[i];
+                    numeros_repetidos[i,1]=numero_veces;
+                    numero_veces=0;
+
+                }
+                int mayor,posicion=0;
+                mayor=numeros_repetidos[0,1];
+                for (int k = 1; k < array_match.Count; k++)
+                {
+                    if(numeros_repetidos[k,1]> mayor){
+                        mayor = numeros_repetidos[k,1];
+                        posicion = k;
+
+                    }
+                    
+                }
+                int indice_ = numeros_repetidos[posicion,0];
+                int vrep_ = numeros_repetidos[posicion,1];
+
+                await turnContext.SendActivityAsync($"Respuesta: {matriz[indice_][0]}.",cancellationToken: cancellationToken);
+            }
+            //Una vez que ya termino.
+            //el numero que mas se repite en el array de matchs
+            //matriz[indice_mas_repetido] es la cadena buscada.
+            //entonces matriz[indice_mas_repetido][0] es la respuesta.
+                     
         }
         
 
